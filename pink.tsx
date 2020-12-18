be6e4aa5921bfcda3ee0d701b9dec43fcc2ff407
Luca Oldenburg <oldenburg@apiomat.com>
2020-12-18 15:36:39 +0100

    chrismer_party setup

---
+++ pink.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pink" tilewidth="32" tileheight="32" tilecount="4608" columns="96">
+ <image source="pics/12_dezember.JPG" trans="ff00ff" width="3088" height="1536"/>
+</tileset>
