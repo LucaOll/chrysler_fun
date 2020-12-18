5d07b81185a6726cdc15741198626cb61998a050
Luca Oldenburg <oldenburg@apiomat.com>
2020-12-18 16:11:29 +0100

    delete again

--- shade.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="shade" tilewidth="32" tileheight="32" tilecount="247" columns="13">
- <image source="pics/pink.jpg" trans="ff00ff" width="446" height="609"/>
-</tileset>
--- sulsul.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="sulsul" tilewidth="64" tileheight="64" tilecount="300" columns="15">
- <image source="pics/sulsul.jpg" trans="ff00ff" width="960" height="1280"/>
-</tileset>
